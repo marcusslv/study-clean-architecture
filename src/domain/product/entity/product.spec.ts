77f616f1ea7cbbb1897cf3c0acf3de9fd47ab250
marcusslv <marcusjapaslv@hotmail.com>
2026-05-30 07:56:04 -0300

    test: adicionar teste para validação de nome vazio e preço negativo no produto

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -13,6 +13,12 @@ describe("Product unit tests", () => {
     }).toThrowError("Name is required");
   });
 
+  it("should throw error when name is empty and price is less than zero", () => {
+    expect(() => {
+      const product = new Product("456", "", -1);
+    }).toThrowError("product: Name is required,product: Price must be greater than zero");
+  });
+
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
